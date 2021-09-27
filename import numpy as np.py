5b826dfc2d6df5daec815cee2991be54d90d4420
jaredwalker-git <74209997+jaredwalker-git@users.noreply.github.com>
2021-09-27 12:45:08 -0400

    datascript
    title

--- import numpy as np.py
+++ import numpy as np.py
@@ -1,18 +1,26 @@
 import numpy as np
 
-arr = []
-arr2 = []
-arr3 =[]
-arr4 = []
+arr = np.array([3, 5 , 'k'])
+arr.resize([1,3])
+arr2 = np.array([1, 2, 'j'])
+arr2.resize([1,3])
 
-arr = [3,4,5]
-arr2 = [1, 2, 3]
-arr3 = ['a', 'b', 'c']
-arr4 = ['d', 'e', 'f']
-
-np.append(arr, arr2)
-arr3.append( arr4)
+arr = np.append(arr, arr2, axis = 0)
 print(arr)
+
+'''
+arr3 = np.array([9, 7 , 'f'])
+arr3.resize([1,3])
+arr4 = np.array([13, 26, 'l'])
+arr4.resize([1,3])
+
+arr3 = np.append(arr3, arr4, axis = 0)
 print(arr3)
-arr.append(arr3)
-print(arr)
+
+#now to append the numpy arrays to a list - one list index for each stock data
+data =[]
+data.append(arr)
+data.append(arr3)
+print(data[0][0,1])
+
+'''
