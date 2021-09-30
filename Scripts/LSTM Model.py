ee9bed5cdcdca0fdc507c3564934b49032e74126
jaredwalker-git <74209997+jaredwalker-git@users.noreply.github.com>
2021-09-30 17:08:30 -0400

    lstm model update
    title

--- Scripts/LSTM Model.py
+++ Scripts/LSTM Model.py
@@ -1,18 +1,17 @@
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 inputData = np.load("inputData.npy", allow_pickle = True)
-print(inputData[1])
-print(inputData[-1])
+#print(inputData[1].shape) --> 1259 for shape
 
-'''
-inputLayer = keras.layers.Input(shape = TBD)
-lstm1 = keras.layers.LSTM(4, activation = 'tanh', recurrent_activation = 'sigmoid')(input) #use of regularizers or initializers? // 4 is for dimentionality of output
-drop1 = keras.layers.Dropout()(lstm1)
-lstm2 = keras.layers.LSTM(4, activation = 'tanh', recurrent_activation = 'sigmoid')(drop1)
+inputLayer = keras.layers.Input(shape = [1, 7])
+lstm1 = keras.layers.LSTM(7, activation = 'tanh', recurrent_activation = 'sigmoid', return_sequences = True)(inputLayer) #use of regularizers or initializers? // 4 is for dimentionality of output
+lstm2 = keras.layers.LSTM(7, activation = 'tanh', recurrent_activation = 'sigmoid')(lstm1)
 dense1 = keras.layers.Dense(100)(lstm2)
 dense2 = keras.layers.Dense(50)(dense1)
 out = keras.layers.Dense(5)(dense2) #will want to change number of nodes to desired output
 
 model = keras.Model(inputs = inputLayer, outputs = out)
 model.summary
-'''
+
