1cf543b71ff709470b5661e4eed9a8d86493a8eb
jaredwalker-git <74209997+jaredwalker-git@users.noreply.github.com>
2021-11-19 21:59:49 -0500

    Debugging continued
    debuggin continued at a point where tensorflow opening libraries but code just spins forever

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,17 +1,3 @@
-import numpy as np
+import itertools
 
-raw_data = np.load('inputData.npy', allow_pickle = True) 
-tickers = ['AAPL']
-#initialize array for data at # tickers x timesteps x features
-input_data = np.zeros(shape = (len(tickers), raw_data[0].shape[0], 5))
-print(len(tickers))
-j = 0
-
-for i in range(len(raw_data)):
-    print(i)
-    if raw_data[i][0, 6] in tickers:
-        print(raw_data[i][0, 6])
-        print(raw_data[i])
-        input_data[j, :, :] = raw_data[i][:, 1:6]
-        j = j + 1
-    
+print(list(itertools.product([0 , 1, 2], repeat = 3)))
