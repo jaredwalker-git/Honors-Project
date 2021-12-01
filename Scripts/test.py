d50d759578fb46a9db8ed4f7595ce1be13e2df4a
jaredwalker-git <74209997+jaredwalker-git@users.noreply.github.com>
2021-12-01 08:18:27 -0500

    results
    title

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,8 +1,20 @@
 import itertools
 import numpy as np
 
-raw_data = np.load("inputData.npy", allow_pickle = True)
+raw_data = np.load('inputData.npy', allow_pickle = True) 
+tickers = ['AAL', 'AAPL']
+#initialize array for data at # tickers x timesteps x features -> features are explicitly set since no good way to generalize this aspect for any data
+input_data = np.zeros(shape = (len(tickers), raw_data[1].shape[0], 5))
+    #j is index for new data, and will be incremented from 0 to num_stocks as tickers are found in raw data
+j = 0
+
 for i in range(len(raw_data)):
-    if raw_data[i].shape[0] == 1258:
-        
-        print(raw_data[i].shape) 
+    if raw_data[i][0, 6] in tickers:
+        input_data[j, :, :] = raw_data[i][:, 1:6]
+        j = j + 1
+
+print(input_data[0,0,:])
+print(input_data[0,-1,:])
+
+print(input_data[1,0,:])
+print(input_data[1,-1,:])
---
+++ Tests/Test 3/README.txt
@@ -0,0 +1,3 @@
+OG Results at 100 Episodes
+Batch Size = 64 
+Gamma = 0.95
